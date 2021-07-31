7f75a80477dbe6de2e1e532db8f84bf28d6d7cfd
donoftime2018 <84360449+donoftime2018@users.noreply.github.com>
2021-07-30 20:47:20 -0400

    Add files via upload

--- sketch6.js
+++ sketch6.js
@@ -1,19 +1,44 @@
 let coord;
+let prompt;
+let cnv;
 
 function setup()
 {
-    createCanvas(1000, 800)
+    cnv = createCanvas(1200, 800)
+    cnv.mousePressed(displayCoordinate)
+    cnv.mouseOver(displayPrompt)
+    cnv.mouseOut(removePrompt)
     background(0, 255, 0)
 }
 
-function mousePressed()
+function displayCoordinate()
 {
     if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height)
     {
+        if (prompt != null)
+            prompt.hide()
+
         if (coord != null)
             coord.remove()
         
         coord = new Coordinate(Math.floor(mouseX), Math.floor(mouseY))
         coord.display()
+    }
+}
+
+function displayPrompt()
+{
+    prompt = createP('Click anywhere in the green area to display the x and y coordinates of the exact place you clicked!')
+    prompt.style('font-family', 'arial')
+    prompt.style('font-size', '25px')
+    prompt.style('text-align', 'center')
+    prompt.position(66, 0)
+}
+
+function removePrompt()
+{
+    if (prompt != null)
+    {
+        prompt.hide()
     }
 }
